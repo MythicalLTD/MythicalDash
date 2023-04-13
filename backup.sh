fb02836dac2f8f5af09ac5db8de7596cda60b824
NaysKutzu <polargaiming@yahoo.com>
2023-04-13 15:27:56 +0200

    Fix

--- backup.sh
+++
@@ -1,7 +0,0 @@
-mysqldump dash > dash.sql
-cd /var/www
-zip -r client.zip client/
-curl --upload-file ./client.zip https://transfer.sh/client.zip -H "Max-Downloads: 2"
-rm client.zip
-cd /var/www/client
-rm dash.sql
