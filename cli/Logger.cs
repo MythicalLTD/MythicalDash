63a1bb4a1ba009fcee2aa46fdccb3d31926bb68c
NaysKutzu <polargaiming@yahoo.com>
2023-08-21 17:35:39 +0000

    PUSH

---
+++ cli/Logger.cs
@@ -0,0 +1,94 @@
+﻿namespace MythicalDash
+{
+    public enum LogType
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    public class Logger
+    {
+        private string logFilePath;
+
+        public Logger()
+        {
+            string logDirectory = "logs";
+            string logFileName = "log.txt";
+            string logDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), logDirectory);
+            logFilePath = Path.Combine(logDirectoryPath, logFileName);
+            Directory.CreateDirectory(logDirectoryPath);
+            RenameLogFile();
+        }
+
+        public void Log(LogType type, string message)
+        {
+            string timestamp = DateTime.Now.ToString("HH:mm:ss");
+            string logText = $"[{timestamp}] [{type.ToString()}] {message}";
+
+            ConsoleColor color = ConsoleColor.White;
+            switch (type)
+            {
+                case LogType.Info:
+                    color = ConsoleColor.Blue;
+                    break;
+                case LogType.Warning:
+                    color = ConsoleColor.Yellow;
+                    break;
+                case LogType.Error:
+                    color = ConsoleColor.Red;
+                    break;
+            }
+            Console.ForegroundColor = color;
+            Console.WriteLine(logText);
+            Console.ResetColor();
+
+            AppendToFile(logText);
+        }
+
+        private void AppendToFile(string logText)
+        {
+            try
+            {
+                using (StreamWriter writer = File.AppendText(logFilePath))
+                {
+                    writer.WriteLine(logText);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing to log file: {ex.Message}");
+            }
+        }
+
+        private void RenameLogFile()
+        {
+            if (File.Exists(logFilePath))
+            {
+                string logFileNameWithoutExtension = Path.GetFileNameWithoutExtension(logFilePath);
+                string logFileExtension = Path.GetExtension(logFilePath);
+#pragma warning disable
+                string logDirectoryPath = Path.GetDirectoryName(logFilePath);
+                string newLogFileName = GetUniqueLogFileName(logDirectoryPath, logFileNameWithoutExtension, logFileExtension);
+#pragma warning restore
+                string newLogFilePath = Path.Combine(logDirectoryPath, newLogFileName);
+                File.Move(logFilePath, newLogFilePath);
+            }
+        }
+
+        private string GetUniqueLogFileName(string directoryPath, string fileNameWithoutExtension, string fileExtension)
+        {
+            string uniqueFileName = $"{fileNameWithoutExtension}-{DateTime.Now:yyyy-MM-dd}{fileExtension}";
+            string uniqueFilePath = Path.Combine(directoryPath, uniqueFileName);
+
+            int counter = 1;
+            while (File.Exists(uniqueFilePath))
+            {
+                uniqueFileName = $"{fileNameWithoutExtension}-{counter++}-{DateTime.Now:yyyy-MM-dd}{fileExtension}";
+                uniqueFilePath = Path.Combine(directoryPath, uniqueFileName);
+            }
+
+            return uniqueFileName;
+        }
+    }
+}
---
+++ cli/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MythicalDash
+{
+    public class Program
+    {
+        public static string ascii = @" 
+  __  __       _   _     _           _ _____            _     
+ |  \/  |     | | | |   (_)         | |  __ \          | |    
+ | \  / |_   _| |_| |__  _  ___ __ _| | |  | | __ _ ___| |__  
+ | |\/| | | | | __| '_ \| |/ __/ _` | | |  | |/ _` / __| '_ \ 
+ | |  | | |_| | |_| | | | | (_| (_| | | |__| | (_| \__ \ | | |
+ |_|  |_|\__, |\__|_| |_|_|\___\__,_|_|_____/ \__,_|___/_| |_|
+          __/ |                                               
+         |___/                                                                                                                 
+";
+        public static string version = "1.0.0";
+        public static bool skiposcheck = true;
+        public static Logger logger = new Logger();
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine(ascii);
+            if (skiposcheck == false) {
+                if (!System.OperatingSystem.IsLinux())
+                {
+                    logger.Log(LogType.Error, "Sorry but this app runs on linux!");
+                    Environment.Exit(0x0);
+                }
+            }
+            if (args.Contains("-version"))
+            {
+                logger.Log(LogType.Info, "You are running version: " + version);
+                Environment.Exit(0x0);
+            }
+            else if (args.Length > 0)
+            {
+                logger.Log(LogType.Error, "This is an invalid startup argument. Please use '-help' to get more information");
+                Environment.Exit(0x0);
+            } else
+            {
+                logger.Log(LogType.Error, "This is an invalid startup argument. Please use '-help' to get more information");
+                Environment.Exit(0x0);
+            }
+        }
+    }
+
+}
