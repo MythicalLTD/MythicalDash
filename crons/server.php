1ac17f0138c87f221d62fecc6c46a5f08146389e
NaysKutzu <ghermancassian2008@gmail.com>
2023-10-17 19:00:47 +0000

    PUSH -> Update composer packages -> Update the crons to work on ubuntu

--- crons/server.php
+++ crons/server.php
@@ -1,15 +1,6 @@
-<?php 
-echo "====== MythicalDash queue ======\n\n";
-echo "[INFO/loader] Loading files...\n";
-try {
-    if (file_exists('../vendor/autoload.php')) { 
-        require("../vendor/autoload.php");
-    } else {
-        die("[INFO/loader] Hello, it looks like you did not run:  'composer install --no-dev --optimize-autoloader'. Please run that and refresh the page");
-    }
-} catch (Exception $e) {
-    die("Hello, it looks like you did not run: 'composer install --no-dev --optimize-autoloader' Please run that and refresh");
-}
+<?php echo "====== MythicalDash queue ======\n\n"; 
+echo "[INFO/loader] Loading files...\n"; 
+require('../vendor/autoload.php');
 include('../functions/passwordgen.php');
 include('../functions/keygen.php');
 include('../functions/encryption.php');
@@ -142,7 +133,7 @@ foreach($queue as $server) {
             'LIT_PACKAGES' => '',
             'JS_FILE' => 'index.js',
             'JARFILE' => 'app.jar',  
-
+            'MAIN_FILE' => 'index.js'
         ),
         'limits' => array(
             'memory' => $server['ram'],
