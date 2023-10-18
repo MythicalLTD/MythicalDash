846280cb7e436a930bdd6fc9b53ea5c9e26aae25
NaysKutzu <ghermancassian2008@gmail.com>
2023-10-18 22:04:36 +0000

    PUSH

--- migrate/11.sql
+++ migrate/11.sql
@@ -1,1 +1,12 @@
-CREATE TABLE `mythicaldash`.`mythicaldash_linkvertise` (`id` INT NOT NULL AUTO_INCREMENT , `skey` TEXT NOT NULL , `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `mythicaldash_linkvertise` (
+  `id` int(11) NOT NULL,
+  `skey` text NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `mythicaldash_linkvertise`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `mythicaldash_linkvertise`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
