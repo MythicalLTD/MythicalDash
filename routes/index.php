b22a50c5a7fdb04c1c8e9457cf7afb1b4cb8173b
NaysKutzu <ghermancassian2008@gmail.com>
2023-10-21 17:26:49 +0000

    PUSH -> Now the main "/" URL is a dashboard redirect, and the errors are being sent to it.

--- routes/index.php
+++ routes/index.php
@@ -1,7 +1,12 @@
 <?php
-$router->add('/', function () {
-    require("../include/main.php");
-    require("../view/index.php");
+$router->add('/', function() {
+    if (isset($_GET['e'])) {
+        header('location: /dashboard?e='. $_GET['e']);
+    } else if (isset($_GET['s'])) {
+        header('location: /dashboard?s='. $_GET['s']);
+    } else {
+        header('location: /dashboard');
+    }
 });
 
 $router->add('/dashboard', function () {
