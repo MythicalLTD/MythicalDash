9be46ecc46b04b936ba7f4c5efa46a7bf9c9da82
NaysKutzu <ghermancassian2008@gmail.com>
2023-10-27 19:04:52 +0000

    PUSH -> Done #83

--- view/requirements/page.php
+++ view/requirements/page.php
@@ -5,4 +5,9 @@ $conn = new Connect();
 $conn = $conn->connectToDatabase();
 $session = new SessionManager();
 $session->authenticateUser();
+if (!$session->getUserInfo('banned') == "") {
+    header('location: /auth/login?e=Sorry but you are banned for using our services');
+    $conn->close();
+    die();
+}
 ?>
