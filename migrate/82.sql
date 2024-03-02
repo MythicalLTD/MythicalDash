d8f47c00e7d6b86384a94db99e81144f26f917f4
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2024-03-02 15:51:20 +0100

    PUSH
    -> Fix it!

--- migrate/82.sql
+++ migrate/82.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `mythicaldash_settings` CHANGE `max_ports` `max_dbs` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '15';
+ALTER TABLE `mythicaldash_settings` CHANGE `max_dbs` `max_dbs` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '15';
